f26f680819b880aa604fdceeebf86f08387c056e
Asaf David <asafd1@gmail.com>
2023-10-02 21:13:05 +0300

    Add ProgressIndicator during login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,22 +3,34 @@ import 'package:shekel/routes/routes.dart';
 import 'package:shekel/util/authentication.dart';
 import 'package:shekel/widgets/app_bar.dart';
 
-class LoginPageWidget extends StatelessWidget {
+class LoginPageWidget extends StatefulWidget {
   const LoginPageWidget({super.key});
+
+  @override
+  State<LoginPageWidget> createState() => _LoginPageWidgetState();
+}
+
+class _LoginPageWidgetState extends State<LoginPageWidget> {
+  bool _inProgress = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ShekelAppBar(),
       body: Center(
-        child: ElevatedButton(
+        child: !_inProgress ? 
+          ElevatedButton(
             onPressed: () {
+              setState(() {
+                _inProgress = true;
+              });
               GoogleAuth().signIn().then((username) {
                 Navigator.popAndPushNamed(context, Routes.home);
               });
             },
             child: const Text('Login'),
-          ),
+          ) : 
+          const CircularProgressIndicator(),
       ),
     );
   }
